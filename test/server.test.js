4c0c569ce01a971828b7355400a675bfb9728917
Merucu <cykps.ps@gmail.com>
2026-06-04 14:49:32 +0200

    fix: remove unused test

--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,8 @@
 import { expect } from 'chai';
 import { describe, it, beforeEach, afterEach } from 'mocha';
-import {
-  InteractionResponseType,
-  InteractionType,
-  InteractionResponseFlags,
-} from 'discord-interactions';
-import { AWW_COMMAND, INVITE_COMMAND } from '../src/commands.js';
+import { InteractionResponseType, InteractionType } from 'discord-interactions';
 import sinon from 'sinon';
 import server from '../src/server.js';
-import { redditUrl } from '../src/reddit.js';
 
 describe('Server', () => {
   describe('GET /', () => {
@@ -57,78 +51,6 @@ describe('Server', () => {
       const response = await server.fetch(request, env);
       const body = await response.json();
       expect(body.type).to.equal(InteractionResponseType.PONG);
-    });
-
-    it('should handle an AWW command interaction', async () => {
-      const interaction = {
-        type: InteractionType.APPLICATION_COMMAND,
-        data: {
-          name: AWW_COMMAND.name,
-        },
-      };
-
-      const request = {
-        method: 'POST',
-        url: new URL('/', 'http://discordo.example'),
-      };
-
-      const env = {};
-
-      verifyDiscordRequestStub.resolves({
-        isValid: true,
-        interaction: interaction,
-      });
-
-      // mock the fetch call to reddit
-      const result = sinon
-        // eslint-disable-next-line no-undef
-        .stub(global, 'fetch')
-        .withArgs(redditUrl)
-        .resolves({
-          status: 200,
-          ok: true,
-          json: sinon.fake.resolves({ data: { children: [] } }),
-        });
-
-      const response = await server.fetch(request, env);
-      const body = await response.json();
-      expect(body.type).to.equal(
-        InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      );
-      expect(result.calledOnce);
-    });
-
-    it('should handle an invite command interaction', async () => {
-      const interaction = {
-        type: InteractionType.APPLICATION_COMMAND,
-        data: {
-          name: INVITE_COMMAND.name,
-        },
-      };
-
-      const request = {
-        method: 'POST',
-        url: new URL('/', 'http://discordo.example'),
-      };
-
-      const env = {
-        DISCORD_APPLICATION_ID: '123456789',
-      };
-
-      verifyDiscordRequestStub.resolves({
-        isValid: true,
-        interaction: interaction,
-      });
-
-      const response = await server.fetch(request, env);
-      const body = await response.json();
-      expect(body.type).to.equal(
-        InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      );
-      expect(body.data.content).to.include(
-        'https://discord.com/oauth2/authorize?client_id=123456789&scope=applications.commands',
-      );
-      expect(body.data.flags).to.equal(InteractionResponseFlags.EPHEMERAL);
     });
 
     it('should handle an unknown command interaction', async () => {
